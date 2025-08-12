0ee36910c761261dc7b2d76c40134614e9a0cc5b
dombyte <78966793+dombyte@users.noreply.github.com>
2025-08-12 13:53:58 +0200

    update

--- scripts/common.sh
+++ scripts/common.sh
@@ -19,3 +19,15 @@ killpid() {
 kill $KEEPALIVE_PID
 }
 
+brewprogram_exists() {
+for cmd in "$@"; do
+    brew list "$cmd" >/dev/null 2>&1 || return 1
+done
+return 0
+}
+command_exists() {
+for cmd in "$@"; do
+    command -v "$cmd" >/dev/null 2>&1 || return 1
+done
+return 0
+}
